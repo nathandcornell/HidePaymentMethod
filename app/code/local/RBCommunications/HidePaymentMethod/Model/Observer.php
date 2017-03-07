fe1b02fe7efe793489175411cc8f205c9a990ff7
Nate Cornell <natec@rb-com.com>
2017-03-06 16:25:09 -0800

    Adds multiselect payment methods

--- app/code/local/RBCommunications/HidePaymentMethod/Model/Observer.php
+++ app/code/local/RBCommunications/HidePaymentMethod/Model/Observer.php
@@ -4,8 +4,10 @@ class RBCommunications_HidePaymentMethod_Model_Observer {
     public function hidePaymentMethod($event){
         $method = $event->getMethodInstance();
         $result = $event->getResult();
+        $code   = $method->getCode();
+        $reportIds = explode(',', Mage::getStoreConfig('hidepayment/hide/methods'));
 
-        if($method->getCode() == 'banktransfer' && !Mage::getIsDeveloperMode()) {
+        if(in_array($method->getCode(), $reportIds) && ! Mage::getIsDeveloperMode()) {
             $result->isAvailable = false;
         }
 
